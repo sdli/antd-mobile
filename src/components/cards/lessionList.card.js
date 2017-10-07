4d3e1d2c7d79450fa03f4d5f2f05d9998f99b893
sdli <sdli@negadna.com>
2017-10-07 19:46:38 +0800

    steven

--- src/components/cards/lessionList.card.js
+++ src/components/cards/lessionList.card.js
@@ -58,7 +58,7 @@ class LessionList extends Component{
                             <Progress percent={34} position="normal" unfilled="show" />
                         </div>
                         {
-                            (UserData && UserData.FinishLesson != CourseData.LessonList.length) &&
+                            (UserData && UserData.FinishLesson == CourseData.LessonList.length) &&
                             <div className={styles.commentLink}>
                                     <p style={{padding:"0 20%"}}><Button size="small" type="ghost" icon={PencilSVG} inline onClick={()=>hashHistory.push("/xinde?CourseId="+CourseId)}>写心得</Button></p>
                                 <p>已完成课程，可以写学习心得！</p>
