7a6c0ee3ff870eeb1ae2c1a547b7b8687649199f
sdli <1669375803@qq.com>
2017-10-12 13:56:28 +0800

    steven

--- src/api/lib/config.js
+++ src/api/lib/config.js
@@ -53,7 +53,7 @@ module.exports = {
         apiPort: 8001,
         package: "teacher",
         filePath: filepath,
-        domain: "apitest.yiaitech.com",
+        domain: "test.api.yiaitech.com",
         protoReqList: dictionary
     },
     production:{
