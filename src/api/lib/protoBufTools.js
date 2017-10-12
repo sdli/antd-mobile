5ee9e830843d3514e05c898a39c4d28b5046d2d2
sdli <1669375803@qq.com>
2017-10-12 14:50:33 +0800

    steven

--- src/api/lib/protoBufTools.js
+++ src/api/lib/protoBufTools.js
@@ -82,7 +82,7 @@ var protoBufferStart = function(reqProtoMessageName,method,data,func){
                 objects: true,  // populates empty objects (map fields) even if defaults=false
                 oneofs: true    // includes virtual oneof fields set to the present field's name
         });
-        console.log(returnObj);
+        console.log(returnObj,"这是返回数据");
         func(returnObj);
     },function(){
         throw "promise rejected";
